85a86199d8ebb3f9652ac57ed63a185cbe65d504
TOMMy-Net <74640949+TOMMy-Net@users.noreply.github.com>
2023-10-23 21:55:16 +0000

    sendmail

--- main.go
+++ main.go
@@ -4,12 +4,70 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
+	"strings"
 	"text/template"
 )
 
+const (
+	yandex      = "smtp.yandex.ru"
+	yandex_port = "465"
+	google      = "smtp.gmail.com"
+	google_port = "587"
+)
+
+type Email struct {
+	Email_field     string
+	Password_field  string
+	Text_field      string
+	Email_field_rec string
+}
+
+func (e Email) SendEmail() {
+	var port string
+	var host string
+	switch (strings.Split(e.Email_field, "@"))[1] {
+	case "yandex.ru":
+		port = yandex_port
+		host = yandex
+	case "gmail.com":
+		port = google_port
+		host = google
+
+	}
+
+	auth := smtp.PlainAuth("", e.Email_field, e.Password_field, host)
+	err := smtp.SendMail(host+":"+port, auth, e.Email_field, []string(e.Email_field_rec), []byte(e.Text_field))
+	if err != nil {
+		log.Println(err)
+		
+		fmt.Println("Sending email...")
+	}
+}
 func SendForm(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" && r.URL.Path == "/action" {
-		fmt.Fprint(w, r.URL.Path)
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		data := Email{
+			Email_field:    r.FormValue("email"),
+			Password_field: r.FormValue("password"),
+			Text_field:     r.FormValue("message"),
+      Email_field_rec: r.FormValue("email2"),
+		}
+		
+		data.SendEmail()
+		fmt.Fprint(w, data)
+	} else {
+		ts, err := template.ParseFiles("static/form.html")
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		ts.Execute(w, nil)
+
 	}
 }
 
